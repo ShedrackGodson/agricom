7d05cb2fd506aa74f3619e65268658cd53e792a6
ShedrackGodson <hedrickgodson23@gmail.com>
2020-08-12 16:08:37 +0300

    configured gis models

--- src/reporter/models.py
+++ src/reporter/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.gis.db import models as geomodel
 
 class Incidences(models.Model):
     name = models.CharField(max_length=20)
-    location = models.CharField(max_length=20)
+    location = geomodel.PointField(srid=4326)
+    objects = geomodel.Manager()
 
     def __str__(self):
         return self.name
