f2b661444ec50ba93d326243d961f8faeeafb6f1
ShedrackGodson <hedrickgodson23@gmail.com>
2020-08-12 18:18:42 +0300

    none

--- src/reporter/load_data.py
+++ src/reporter/load_data.py
@@ -1,4 +1,6 @@
-
+import os
+from django.contrib.gis.utils import LayerMapping
+from .models import Districts
 
 # Auto-generated `LayerMapping` dictionary for Districts model
 districts_mapping = {
@@ -6,3 +8,9 @@ districts_mapping = {
     'district_n': 'District_N',
     'geom': 'MULTIPOLYGON',
 }
+
+districts_shp = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/Districts.shp'))
+
+def run(verbose=True):
+    lm = LayerMapping(Districts, districts_shp, districts_mapping, transform=False, encoding="iso-8859-1")
+    lm.save(strict=True, verbose=verbose)
