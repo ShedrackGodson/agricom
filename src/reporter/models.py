f0c6558323ea7cbf3e388ed2e4ad20c215b93614
ShedrackGodson <hedrickgodson23@gmail.com>
2020-08-12 11:41:13 +0300

    added app reporter and incidences model

---
+++ src/reporter/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Incidences(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
