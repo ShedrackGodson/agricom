b9b5eb9563eaadea279799e9cae96d97183ef238
ShedrackGodson <hedrickgodson23@gmail.com>
2020-08-14 11:46:26 +0300

    rendered incidences datasets to the Map

--- src/reporter/urls.py
+++ src/reporter/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, wards_datasets
+from .views import HomeView, wards_datasets, incidences_datasets
 
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name="home"),
     path('wards_datasets/', wards_datasets, name="wards_datasets"),
+    path('incidences_datasets/', incidences_datasets, name="incidences_datasets"),
 ]
--- src/reporter/views.py
+++ src/reporter/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from .models import Wards
+from .models import Wards, Incidences
 from django.http import HttpResponse
 from django.core.serializers import serialize
 
@@ -12,3 +12,8 @@ class HomeView(TemplateView):
 def wards_datasets(request):
     wards = serialize('geojson', Wards.objects.all())
     return HttpResponse(wards, content_type='json')
+
+
+def incidences_datasets(request):
+    incidences = serialize('geojson', Incidences.objects.all())
+    return HttpResponse(incidences, content_type='json')
